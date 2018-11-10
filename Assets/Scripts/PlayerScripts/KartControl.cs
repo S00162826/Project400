7fb4254158e6428db25e3a867fd03ac36b8fde7b
S00162826 <S00162826@mail.itsligo.ie>
2018-11-10 23:43:18 +0000

    New Kart Control Class
    Removed old player controller class
    Completely New Script for movement
    Updated Player Model

---
+++ Assets/Scripts/PlayerScripts/KartControl.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KartControl : MonoBehaviour
+{
+    Rigidbody rb;
+    public Vector3 centreOfMass = new Vector3(0,0,0);
+
+    public WheelCollider[] wc;
+
+    public int accelerationLength;
+
+    public bool brakeAllowed;
+
+    public float maxSpeed = 2500f;
+    public float maxSteer = 25f;
+    public float maxBrake = 10000f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centreOfMass;
+    }
+
+     void Update()
+    {
+        Handbrake();
+    }
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < accelerationLength; i++)
+        {
+            wc[i].motorTorque = Input.GetAxis("Vertical") * maxSpeed;
+        }
+
+        wc[0].steerAngle = Input.GetAxis("Horizontal") * maxSteer;
+        wc[1].steerAngle = Input.GetAxis("Horizontal") * maxSteer;
+    }
+
+    void Handbrake()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            brakeAllowed = true;
+        }
+        else
+        {
+            brakeAllowed = false;
+        }
+
+        if (brakeAllowed)
+        {
+            for (int i = 0; i < accelerationLength; i++)
+            {
+                wc[i].brakeTorque = maxBrake;
+                wc[i].motorTorque = 0;
+            }
+        }
+        else if (!brakeAllowed && Input.GetButton("Vertical") == true)
+        {
+            for (int i = 0; i < accelerationLength; i++)
+            {
+                wc[i].brakeTorque = 0;
+            }
+        }
+    }
+}
